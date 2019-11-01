352f14ca2fa444f48b95e2dc2a4b096a26fbb27d
Tr-blank <blank0129@gmail.com>
2019-11-01 19:12:33 +0800

    :zap: Update: add css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template lang="pug">
   #app
-    ul
-      li
-        span 單字
-        span 解釋
-        span 接觸緣由
-        span 標籤
-      li(v-for="(item, index) in words" :key="index")
-        span {{item.word}}
-        span {{item.translation}}
-        span {{item.motive}}
-        span {{item.tag}}
+    .word__list
+      .word__item
+        span.word__english 單字
+        span.word__chinese 解釋
+        span.word__motive 接觸緣由
+        span.word__tag 標籤
+      from.word__item
+        label.word__english
+          input.word__text(type="text")
+        label.word__chinese
+          input.word__text(type="text")
+        label.word__motive
+          input.word__text(type="text")
+        label.word__tag
+          input.word__text(type="text")
+      .word__item(v-for="(item, index) in words" :key="index")
+        .word__english  {{item.word}}
+        .word__chinese {{item.translation}}
+        .word__motive {{item.motive}}
+        .word__tag {{item.tag}}
   </div>
 </template>
 
 <script>
 import { wordCollection } from './db'
+
+import '@/assets/styles/normalize.css'
+import '@/assets/styles/reset.css'
 
 export default {
   name: 'app',
@@ -63,12 +75,43 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-#app {
+#app
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+
+.word
+  &__list
+    margin: 0 auto;
+    padding: 0;
+    max-width: 1440px;
+  &__item
+    display: flex;
+    border: 1px solid #eee;
+    border-radius: 3px;
+    padding: 10px;
+  &__text
+    width: 100%;
+  &__text,
+  &__english,
+  &__chinese,
+  &__motive,
+  &__tag
+    padding: 5px;
+  &__english
+    width: 50%;
+    font-size 3rem
+    text-align: right;
+  &__chinese
+    width: 50%;
+    font-size 2rem
+  &__motive
+    width: 100%;
+  &__tag
+    width: 100%;
+    font-size 0.6rem
+
 </style>
